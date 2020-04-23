59563759112de925d2447aba186ea48ebf080bda
Lee Henkel <lhenkel@gmail.com>
2020-04-23 10:08:30 -0600

    Update from original

--- libs/debug-lib.js
+++ libs/debug-lib.js
@@ -8,35 +8,35 @@ let logs;
 let timeoutTimer;
 
 export function init(event, context) {
-    logs = [];
+  logs = [];
 
-    // Log API event
-    debug("API event", {
-        body: event.body,
-        pathParameters: event.pathParameters,
-        queryStringParameters: event.queryStringParameters,
-    });
+  // Log API event
+  debug("API event", {
+    body: event.body,
+    pathParameters: event.pathParameters,
+    queryStringParameters: event.queryStringParameters,
+  });
 
-    // Start timeout timer
-    timeoutTimer = setTimeout(() => {
-        timeoutTimer && flush(new Error("Lambda will timeout in 100 ms"));
-    }, context.getRemainingTimeInMillis() - 100);
+  // Start timeout timer
+  timeoutTimer = setTimeout(() => {
+    timeoutTimer && flush(new Error("Lambda will timeout in 100 ms"));
+  }, context.getRemainingTimeInMillis() - 100);
 }
 
 export function end() {
-    // Clear timeout timer
-    clearTimeout(timeoutTimer);
-    timeoutTimer = null;
+  // Clear timeout timer
+  clearTimeout(timeoutTimer);
+  timeoutTimer = null;
 }
 
 export function flush(e) {
-    logs.forEach(({ date, string }) => console.debug(date, string));
-    console.error(e);
+  logs.forEach(({ date, string }) => console.debug(date, string));
+  console.error(e);
 }
 
 export default function debug() {
-    logs.push({
-        date: new Date(),
-        string: util.format.apply(null, arguments),
-    });
+  logs.push({
+    date: new Date(),
+    string: util.format.apply(null, arguments),
+  });
 }
